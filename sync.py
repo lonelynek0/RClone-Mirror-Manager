c513e00b00bbf5a194adfd8d416de0f4f328b75f
lonelynek0 <100769175+lonelynek0@users.noreply.github.com>
2022-04-15 08:48:24 +0300

    Update sync.py

--- sync.py
+++ sync.py
@@ -6,7 +6,7 @@ import rclone
 
 cfg_path = "./rclone.conf"
 from_drives = [
-    "test",
+    "Aniex_Air",
     "___backups",
     "__code",
     "__meow",
@@ -16,7 +16,7 @@ from_drives = [
     "public",
 ]
 to_drives = [
-    "1",
+    "xbackup",
     "2",
     "3",
     "4",
